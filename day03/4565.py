b3d9cae80cdb6fea795f7361c9594aba4289e677
刘子勤 <563233169@qq.com>
2019-05-31 18:17:47 +0800

    k

---
+++ day03/4565.py
@@ -0,0 +1,11 @@
+def ll_pp():
+    for i in range(1,10):
+        c= i+1
+        for j in  range(1s,c):
+            print('%s * %s =%s'%(j,i,j*i),end=' ')
+        print('')
+
+if __name__ == '__main__':
+       ll_pp()
+
+s
